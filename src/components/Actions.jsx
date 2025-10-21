429eae028f2b45a5755e0484a4939897f613edfc
Cristian <cristianrodrigues93@gmail.com>
2025-10-21 18:46:10 -0300

    PORRA VERCEL VSFD

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,19 +1,14 @@
-import Wpp from '../assets/wpp.png';
-import Insta from '../assets/insta.png';
-
 function Actions() {
   return (
     <div className="actions">
       <a className="btn" href="https://www.instagram.com/rodrigues_gabriel051/" target="_blank">
-        <img src={Insta} alt="Instagram" className="icon" />
+        <img src="/insta.png" alt="Instagram" className="icon" />
         Instagram
       </a>
       <a className="btn" href="https://wa.link/tyi17g" target="_blank">
-        <img src={Wpp} alt="WhatsApp" className="icon" />
+        <img src="/wpp.png" alt="WhatsApp" className="icon" />
         WhatsApp
       </a>
     </div>
   );
 }
-
-export default Actions;
