475e662d1ea4d312a942d5c92a6681c61f524ba8
Cristian <cristianrodrigues93@gmail.com>
2025-10-21 19:25:33 -0300

    ultima tentativa de ajustar os icons

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -23,11 +23,11 @@ function Intro() {
 
      <div class="actions">
          <a class="btn" href="https://www.instagram.com/rodrigues_gabriel051/" target="_blank">
-             <img src="../src/assets/insta.png" alt="Instagram" class="icon"/>
+             <img src="/insta.png" alt="Instagram" class="icon"/>
              Instagram
          </a>
          <a class="btn" href="https://wa.link/tyi17g" target="_blank">
-             <img src="../src/assets/wpp.png" alt="WhatsApp" class="icon"/>
+             <img src="/wpp.png" alt="WhatsApp" class="icon"/>
              WhatsApp
          </a>
      </div>
