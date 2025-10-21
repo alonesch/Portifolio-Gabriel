b72ca4671a7a3649c21c48d12813f5c042227798
Cristian <cristianrodrigues93@gmail.com>
2025-10-21 18:41:19 -0300

    Ajuste PATH ancoras

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,22 +1,19 @@
-import Wpp from '../assets/Wpp.png';
-import Insta from '../assets/Insta.png'
+import Wpp from '../assets/wpp.png';
+import Insta from '../assets/insta.png';
+
 function Actions() {
-    
-
-    return (
-
-  <div className="actions">
-      <a class="btn" href="https://www.instagram.com/rodrigues_gabriel051/" target="_blank">
-          <img src={Insta} alt="Instagram" className="icon" />
-          Instagram
+  return (
+    <div className="actions">
+      <a className="btn" href="https://www.instagram.com/rodrigues_gabriel051/" target="_blank">
+        <img src={Insta} alt="Instagram" className="icon" />
+        Instagram
       </a>
-      <a class="btn" href="https://wa.link/tyi17g" target="_blank">
-          <img src={Wpp} alt="WhatsApp" className="icon" />
-          WhatsApp
+      <a className="btn" href="https://wa.link/tyi17g" target="_blank">
+        <img src={Wpp} alt="WhatsApp" className="icon" />
+        WhatsApp
       </a>
-
     </div>
-    );
+  );
 }
 
 export default Actions;
